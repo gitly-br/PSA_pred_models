55623e601189e3d1bb0f9c36c8260f623aae348e
Gabriel Rangel <gabrielmrangel7@gmail.com>
2024-11-19 15:21:56 -0300

    adding inference + data_normalization

--- psa_models_back/source/app/sanic_blueprints/modelo_1/modelo_1.py
+++ psa_models_back/source/app/sanic_blueprints/modelo_1/modelo_1.py
@@ -1,7 +1,15 @@
 from sanic import Blueprint, response
+from app.utils.openweather import get_OW_mongo_data_and_agg
+from app.utils.inferencia import inferencia_previsao
 
 bp_modelo_1 = Blueprint('modelo_1', url_prefix='/modelo_1')
 
-@bp_modelo_1.route('/', methods=['GET'])
+@bp_modelo_1.route('/', methods=['POST'])
 async def get_modelo_1(request):
-    return response.json({'message': 'Hello from modelo_1!'})
+
+    args = request.json
+
+    resp = await inferencia_previsao(request, args.get('modelo', "xgboost_sa_1"), args.get('regiao', 'SA'))
+
+
+    return response.json({'status' : 'success', 'data' : resp})
---
+++ psa_models_back/source/app/utils/inferencia.py
@@ -0,0 +1,30 @@
+from app.utils.openweather import get_OW_mongo_data_and_agg
+
+async def inferencia_previsao(request, modelo, regiao: str):
+
+    # Pega dados do modelo
+
+    success, conn_error, region_models, err_msg = await request.app.ctx.mongo_obj.read_one(
+        db_name='models_db',
+        col='models_col',
+        filter_={'region_name' : regiao.upper()}
+        )
+    
+    model_infos = {}
+
+    for model, agg_info in region_models.get('models',[]).items():
+        if model == modelo:
+            model_infos[model] = agg_info
+            break
+
+    list_data = []
+    print(model_infos.items())
+
+    
+    models_agg = model_infos[modelo]
+    for data_source, agg_info in models_agg.items():
+
+        if agg_info != {}:
+            list_data.append(await get_OW_mongo_data_and_agg(request, agg_info))
+
+    return {'status' : 'success', 'data' : {'score' : 0.5, 'regiao' : regiao, 'modelo' : modelo}}
