03876ae1c6c4da9ace3747ae91c06b5f51e6c500
Gabriel Rangel <gabrielmrangel7@gmail.com>
2024-11-19 15:21:56 -0300

    adding inference + data_normalization

--- psa_models_back/source/app/utils/openweather.py
+++ psa_models_back/source/app/utils/openweather.py
@@ -1,6 +1,8 @@
 import asyncio
 from datetime import datetime
+import pandas as pd
 
+from app.utils.aggregator import create_agg_dict
 
 async def get_openweather_api_data(app, route, params=None):
         
@@ -22,3 +24,54 @@ async def get_openweather_api_data(app, route, params=None):
         
         
         await asyncio.sleep(3600*1.45)
+
+
+async def get_OW_mongo_data_and_agg(request, agg_config):
+
+
+    success, conn_prob, openw_data, err_msg = await request.app.ctx.mongo_obj.read_all(db_name='api_data', col='openweather_col', sort=[('dt_request', -1)])
+
+    data = openw_data[0]['list']
+
+
+    flattened_data = []
+    for entry in data:
+        flat_entry = {
+            "dt": datetime.fromtimestamp(entry.get("dt")),
+            "temp": entry["main"].get("temp"),
+            "feels_like": entry["main"].get("feels_like"),
+            "temp_min": entry["main"].get("temp_min"),
+            "temp_max": entry["main"].get("temp_max"),
+            "pressure": entry["main"].get("pressure"),
+            "sea_level": entry["main"].get("sea_level"),
+            "grnd_level": entry["main"].get("grnd_level"),
+            "humidity": entry["main"].get("humidity"),
+            "temp_kf": entry["main"].get("temp_kf"),
+            "weather_main": entry["weather"][0].get("main") if entry.get("weather") else None,
+            "weather_description": entry["weather"][0].get("description") if entry.get("weather") else None,
+            "weather_icon": entry["weather"][0].get("icon") if entry.get("weather") else None,
+            "clouds_all": entry["clouds"].get("all"),
+            "wind_speed": entry["wind"].get("speed"),
+            "wind_deg": entry["wind"].get("deg"),
+            "wind_gust": entry["wind"].get("gust"),
+            "visibility": entry.get("visibility"),
+            "pop": entry.get("pop"),
+            "rain_3h": entry["rain"].get("3h", 0) if "rain" in entry else 0,
+            "sys_pod": entry["sys"].get("pod"),
+            "dt_txt": datetime.fromisoformat(entry.get("dt_txt"))
+        }
+        flattened_data.append(flat_entry)
+
+    # Criar o DataFrame
+    df = pd.DataFrame(flattened_data)
+    agg_config_final = {}
+    for i, j in agg_config.items():
+        agg_config_final[i] = (j['time'], tuple(j['aggs']))
+
+    print(agg_config_final)
+    
+    df_agg = df.groupby(pd.Grouper(key='dt', freq='D')).agg(
+                                                  **create_agg_dict(agg_config_final)
+                                                  ).reset_index()
+
+    print(df_agg)
