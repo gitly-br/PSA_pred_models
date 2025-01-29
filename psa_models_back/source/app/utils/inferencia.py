2cb945dc235911888fb61ef531ec62e7c3ebb8d1
Gabriel Rangel <gabrielmrangel7@gmail.com>
2025-01-29 11:04:06 -0300

    new function to get all models prediction for region

--- psa_models_back/source/app/utils/inferencia.py
+++ psa_models_back/source/app/utils/inferencia.py
@@ -57,7 +57,7 @@ async def inferencia_previsao(request, modelo:str, regiao: str, dt_request=None)
 
     return True, result
 
-async def inferencia_previsao_2(request, modelo:str, regiao: str, dt_request=None):
+async def inferencia_previsao_2(request, regiao: str, dt_request=None):
 
     # Pega dados do modelo
 
@@ -70,6 +70,8 @@ async def inferencia_previsao_2(request, modelo:str, regiao: str, dt_request=Non
     model_infos = {}
 
     list_data = []
+    
+    resp_list = []
 
     for model, agg_info in region_models.get('models',{}).items():
 
@@ -90,33 +92,31 @@ async def inferencia_previsao_2(request, modelo:str, regiao: str, dt_request=Non
         # Fazer inferencia e salvar em mongo
         # Load the model
 
-        resp_list = []
+        # for weather_data in list_data:
 
-        for weather_data in list_data:
+        if weather_data[0] == False:
+            resp_list.append({'status' : 0, 'proba' : None, 'predict' : None, 'score' : None,'region' : regiao, 'lat' : model_infos[model].get('region_coord', [-23.699012, -46.4537949])[0], 'lon': model_infos[model].get('region_coord', [-23.699012, -46.4537949])[1], 'circle_rad' : 0, 'model' : model, 'obj_version':'1.0', 'dt_inference' : datetime.now()})
 
-            if weather_data[0] == False:
-                resp_list.append({'status' : 0, 'proba' : None, 'predict' : None, 'score' : None,'region' : regiao, 'lat' : model_infos[model].get('region_coord', [-23.699012, -46.4537949])[0], 'lon': model_infos[model].get('region_coord', [-23.699012, -46.4537949])[1], 'circle_rad' : 0, 'model' : model, 'obj_version':'1.0', 'dt_inference' : datetime.now()})
-
-            loaded_model = joblib.load(f'/source/app/utils/{model}.joblib')
+        loaded_model = joblib.load(f'/source/app/utils/{model}.joblib')
+        
+        day_row = weather_data[2].iloc[[1]]
             
-            day_row = weather_data[2].iloc[[1]]
-                
-            # Convert the row to a NumPy array
-            input_data = day_row.drop(columns=['dt']).values
+        # Convert the row to a NumPy array
+        input_data = day_row.drop(columns=['dt']).values
 
-            # Make a prediction
-            prediction = loaded_model.predict(input_data)
-            proba = loaded_model.predict_proba(input_data)
-            
-            result = {'status' : 1,'proba' : proba[0][1], 'predict' : int(prediction[0]), 'score' : 0.25, 'region' : regiao, 'lat' : model_infos[model].get('region_coord', [-23.699012, -46.4537949])[0], 'lon': model_infos[model].get('region_coord', [-23.699012, -46.4537949])[1], 'circle_rad' : model_infos[model].get('circle_rad', 100), 'model' : model, 'obj_version':'1.0', 'dt_inference' : datetime.now()}	
+        # Make a prediction
+        prediction = loaded_model.predict(input_data)
+        proba = loaded_model.predict_proba(input_data)
+        
+        result = {'status' : 1,'proba' : proba[0][1], 'predict' : int(prediction[0]), 'score' : 0.25, 'region' : regiao, 'lat' : model_infos[model].get('region_coord', [-23.699012, -46.4537949])[0], 'lon': model_infos[model].get('region_coord', [-23.699012, -46.4537949])[1], 'circle_rad' : model_infos[model].get('circle_rad', 100), 'model' : model, 'obj_version':'1.0', 'dt_inference' : datetime.now()}	
 
 
-            success, conn_error, err_msg = await request.app.ctx.mongo_obj.write_one(
-                db_name='models_db',
-                col='inference_col',
-                doc=result
-            )
+        success, conn_error, err_msg = await request.app.ctx.mongo_obj.write_one(
+            db_name='models_db',
+            col='inference_col',
+            doc=result
+        )
 
-            resp_list.append({'modelo' : modelo, 'result' : result})
+        resp_list.append({'modelo' : model, 'result' : result})
 
     return True, resp_list
