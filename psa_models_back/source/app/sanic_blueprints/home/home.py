833becfd2a869a0702c60340502e97095fbece54
Gabriel Rangel <gabrielmrangel7@gmail.com>
2025-01-29 16:05:09 -0300

    new route to dash

--- psa_models_back/source/app/sanic_blueprints/home/home.py
+++ psa_models_back/source/app/sanic_blueprints/home/home.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from sanic import Blueprint, response
 from app.utils.openweather import get_OW_mongo_data_and_agg
-from app.utils.inferencia import inferencia_previsao_2
+from app.utils.inferencia import inferencia_geral, inferencia_previsao_2
 from app.utils.encode_json import encode_body
 
 bp_home = Blueprint('home', url_prefix='/home')
@@ -12,8 +13,14 @@ async def get_home(request):
 
     if args is None:
         args = {}
+    
+    
+    #TODO define datetime value
 
-    succ, resp = await inferencia_previsao_2(request, "a", 'meninos', args.get('dt_request', None))
+    regions = args.get('regions', ['SA', 'MENINOS', 'ORATORIO', 'TAMCENTRAL', 'GUARARA'])
+
+    succ, resp = await inferencia_geral(request, regions, args.get('dt_request', datetime.now().isoformat()))
+
 
     if succ:
         return response.json({'status' : 'success', 'data' : encode_body(resp)})
