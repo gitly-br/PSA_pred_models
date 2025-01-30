f6ce31330641b187f7a70d8c33d481b93661ded0
Gabriel Rangel <gabrielmrangel7@gmail.com>
2025-01-30 10:05:14 -0300

    corect value

--- streamlit_app/home.py
+++ streamlit_app/home.py
@@ -118,7 +118,7 @@ with col2:
         st.markdown(
         f"""
         <div style='display: flex; flex-direction: column; align-items: center; justify-content: center;margin-bottom: 10px;'>
-            <p style='font-size: 20px; font-family: "Source Sans Pro", sans-serif; font-weight: 600; text-align: center; color: #333; margin: 10px 0; line-height: 1.2;'>Amanhã<br>{st.session_state.next_predict_date}</p>
+            <p style='font-size: 20px; font-family: "Source Sans Pro", sans-serif; font-weight: 600; text-align: center; color: #333; margin: 10px 0; line-height: 1.2;'>Amanhã<br>{st.session_state.predict_date}</p>
             <div style='background-color: rgba{str(get_color(data_list_summary['SA']['proba']))}; width: 60px; height: 60px; border-radius: 50%; display: flex; justify-content: center; align-items: center;'>
             </div>
         </div>
@@ -129,7 +129,7 @@ with col2:
 
 with col3:
     with st.container(border=True):
-        st.markdown(f"<div style=display: flex; justify-content: center; align-items: center;'><h5 style='text-align: center;'>{data_list_summary['SA']['proba']*100:.1f}% de Possibilidade de Alagamento ou Inundação em Santo André em<br>{st.session_state.next_predict_date}</h5></div>", unsafe_allow_html=True)
+        st.markdown(f"<div style=display: flex; justify-content: center; align-items: center;'><h5 style='text-align: center;'>{data_list_summary['SA']['proba']*100:.1f}% de Possibilidade de Alagamento ou Inundação em Santo André em<br>{st.session_state.predict_date}</h5></div>", unsafe_allow_html=True)
 
 st.divider()
 st.markdown("<h3 style='color:green;'>Modelo de Bacias:</h3>", unsafe_allow_html=True)
