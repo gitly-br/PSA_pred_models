2a38213f54000b54c8a1b1d0984d45914bbc946d
Gabriel Rangel <gabrielmrangel7@gmail.com>
2025-02-28 14:58:10 -0300

    fix api url

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,7 +8,7 @@ import folium
 import json
 from datetime import datetime, timedelta
 
-api_url = environ.get('API_URL_', 'http://psa_models_back:8000')
+api_url = environ.get('API_URL', 'http://psa_models_back:8000')
 
 def call_models(dt_begin=None):
         
