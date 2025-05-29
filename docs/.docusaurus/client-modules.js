3b0150c4dbe6087590b8e18805c7c6f6d5e840ee
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-05-29 17:27:37 -0300

    docs: remove .docusaurus dir from version control

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/rnicola/Documents/PSA/refactor/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/rnicola/Documents/PSA/refactor/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/rnicola/Documents/PSA/refactor/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/rnicola/Documents/PSA/refactor/docs/src/css/custom.css"),
-];
