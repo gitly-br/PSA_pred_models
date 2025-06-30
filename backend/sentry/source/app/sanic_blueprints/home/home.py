ab9b5eecf42de8e144f28b7aebcb24266c4b5991
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-06-30 00:37:02 -0300

    backend: fix small makefile issue

--- backend/sentry/source/app/sanic_blueprints/home/home.py
+++ backend/sentry/source/app/sanic_blueprints/home/home.py
@@ -1,20 +1,7 @@
-from datetime import datetime
 from sanic import Blueprint, response
 
 bp_home = Blueprint('home', url_prefix='/home')
 
-@bp_home.route('/', methods=['POST'])
+@bp_home.route('/', methods=['POST', 'GET'])
 async def get_home(request):
-
-    args = request.json
-
-    if args is None:
-        args = {}
-    
-    
-    #TODO define datetime value
-
-    regions = args.get('regions', ['SA', 'MENINOS', 'ORATORIO', 'TAMCENTRAL', 'GUARARA'])
-
-    # Placeholder for removed inference logic
-    return response.json({'status' : 'success', 'data' : f"This is a placeholder for the home route. Regions: {regions}"})
+    return response.json({'status': 'deprecated', 'message': 'This endpoint is deprecated. Please use /region/{region_name} instead.'}, status=404)
