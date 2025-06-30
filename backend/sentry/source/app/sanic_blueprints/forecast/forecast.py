01cbcd6cf4a7cab242d87caca94d89e184049f2e
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-06-30 00:37:02 -0300

    backend: fix small makefile issue

--- backend/sentry/source/app/sanic_blueprints/forecast/forecast.py
+++ backend/sentry/source/app/sanic_blueprints/forecast/forecast.py
@@ -2,6 +2,6 @@ from sanic import Blueprint, response
 
 bp_forecast = Blueprint("forecast", url_prefix="/forecast")
 
-@bp_forecast.get("/")
+@bp_forecast.route('/', methods=['POST', 'GET'])
 async def get_forecast(request):
-    return response.json({"status": "success", "data": "This is a placeholder for the forecast route."})
+    return response.json({'status': 'deprecated', 'message': 'This endpoint is deprecated. Please use /region/{region_name} instead.'}, status=404)
