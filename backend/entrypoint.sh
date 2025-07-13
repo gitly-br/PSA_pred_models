2ddc57672759187a87dd134d2a56af2b7a359158
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-07-13 05:18:04 -0300

    backend: update main dockerfile and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-export MONGO_URI=${MONGO_URI}
-export OPENWEATHER_API_KEY=${OPENWEATHER_API_KEY}
-# Dump runtime env vars to a file cron can source
-printenv | grep -v "no_proxy" > /etc/cron.env
+# Create crontab with current environment variables
+cat > /etc/cron.d/crontab << EOF
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+MONGO_URI=${MONGO_URI}
+OPENWEATHER_API_KEY=${OPENWEATHER_API_KEY}
+
+*/1 * * * * root psa-harvest >> /var/log/cron.log 2>&1
+*/1 * * * * root psa-floodcast >> /var/log/cron.log 2>&1
+EOF
+
+chmod 0644 /etc/cron.d/crontab
 
 # Start cron and tail its log
 cron && tail -f /var/log/cron.log &
