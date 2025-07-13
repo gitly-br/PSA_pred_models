147fdf14e84686d4ab500b53ad4a8a8284dc02f6
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-07-13 07:42:32 -0300

    backend: update dockerfile and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,11 +6,16 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 MONGO_URI=${MONGO_URI}
 OPENWEATHER_API_KEY=${OPENWEATHER_API_KEY}
 
-*/1 * * * * root psa-harvest >> /var/log/cron.log 2>&1
-*/1 * * * * root psa-floodcast >> /var/log/cron.log 2>&1
+*/30 * * * * root psa-harvest >> /var/log/cron.log 2>&1
+5,35 0 * * * root psa-floodcast >> /var/log/cron.log 2>&1
+0 1,2,4,6,9,12 * * * root psa-floodcast >> /var/log/cron.log 2>&1
 EOF
 
 chmod 0644 /etc/cron.d/crontab
+
+# Kill any existing cron processes and clean up PID file
+pkill cron || true
+rm -f /var/run/crond.pid
 
 # Start cron and tail its log
 cron && tail -f /var/log/cron.log &
